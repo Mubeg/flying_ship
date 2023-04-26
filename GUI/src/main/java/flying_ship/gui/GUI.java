6cde7e8dabe678894befae05931f457f0ef2b1c1
nerett <pt-mvb@inbox.lv>
2023-04-26 20:25:07 +0300

    initial

---
+++ GUI/src/main/java/flying_ship/gui/GUI.java
@@ -0,0 +1,13 @@
+package flying_ship.gui;
+
+/**
+ *
+ * @author nerett
+ */
+
+public class GUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
