08db9d5e465d1a010a5ef0e6629372482492d6c2
Mubeg <nikitadanilov.2001@mail.ru>
2023-05-17 19:12:01 +0300

    Constants

--- Backend/constants.h
+++ Backend/constants.h
@@ -7,14 +7,14 @@ using namespace std::literals::chrono_literals;
 #define UNIQUE_IDS_N 4000
 #define DEFAULT_AGENT_SIZE 30
 #define BACKEND_SLEEP_TIME 200ms
-#define DEFAULT_SCREEN_WIDTH 800
-#define DEFAULT_SCREEN_HEIGHT 600
+#define DEFAULT_SCREEN_WIDTH 1280
+#define DEFAULT_SCREEN_HEIGHT 720
 #define DEFAULT_TIME_TICK_SPEED 200ms
-#define ENV_PAUSE_SLEEP 1000ms
-#define AGENT_SIZE 10
+#define ENV_PAUSE_SLEEP 100ms
+#define AGENT_SIZE 40
 #define LAYER_HEIGHT 20
 #define MIN_SIZE 10
-#define MAX_SIZE 40
+#define MAX_SIZE 50
 #define MAX_TRIES 100
 #define SHIFT 2
 #define AGENT_Y_PADDING (DEFAULT_SCREEN_HEIGHT - 20)
